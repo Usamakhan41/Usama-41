c3ed18601d5b4e73953c530164839ce9972ef184
Usamakhan41 <49385245+Usamakhan41@users.noreply.github.com>
2019-04-09 08:00:08 +0500

    Add files via upload

---
+++ Castingvariable.java
@@ -0,0 +1,10 @@
+class Casting{
+public static void main(String[] args){
+double d=20.50;
+long l=(long)d;
+double a=d-l;
+System.out.println(d);
+System.out.println(l);
+System.out.println(a);
+}
+}
---
+++ Print.java
@@ -0,0 +1,12 @@
+class Print{
+public static void main(String[] args){
+String name="Muhammad Usama";
+int marks=550;
+String status="Pass";
+int per=98;
+System.out.println(name);
+System.out.println(marks);
+System.out.println(status);
+System.out.println(per+"%");
+}
+}
